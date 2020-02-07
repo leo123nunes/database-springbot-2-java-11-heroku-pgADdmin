3c18d3ee3830cb9a48717df9cdd42b8743df1c53
leo123nunes <ti.userleo@hotmail.com>
2020-02-07 17:24:28 -0300

    H2 database

--- src/main/java/com/project/course/CourseApplication.java
+++ src/main/java/com/project/course/CourseApplication.java
@@ -1,13 +1,25 @@
 package com.project.course;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.project.course.entities.User;
+import com.project.course.resources.UserResource;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runner(UserResource resource) {
+		return args ->{
+			resource.save(new User(null,"Maria","maria@gmail.com","99999999","123456"));
+		};
 	}
 
 }
--- src/main/java/com/project/course/resources/UserResource.java
+++ src/main/java/com/project/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.project.course.resources;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,14 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.course.entities.User;
 
-@RestController
-@RequestMapping(value = "/users")
-public class UserResource {
+
+public interface UserResource extends CrudRepository<User,Long> {
 	
 	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Maria","maria@gmail.com","99999999","123456");
-		return ResponseEntity.ok().body(u);
-	}
 }
