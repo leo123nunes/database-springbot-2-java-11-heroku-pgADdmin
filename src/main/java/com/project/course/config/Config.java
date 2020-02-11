89cfe9b81c7cf5c0a80912e1fff820311ceb6af7
leo123nunes <ti.userleo@hotmail.com>
2020-02-11 20:02:23 -0300

    Payment one to one association

--- src/main/java/com/project/course/config/Config.java
+++ src/main/java/com/project/course/config/Config.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.project.course.entities.Category;
 import com.project.course.entities.Order;
 import com.project.course.entities.OrderItem;
+import com.project.course.entities.Payment;
 import com.project.course.entities.Product;
 import com.project.course.entities.User;
 import com.project.course.entities.enums.OrderStatus;
@@ -49,6 +50,18 @@ public class Config implements CommandLineRunner {
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategory().add(cat2);
+		p2.getCategory().add(cat1);
+		p2.getCategory().add(cat3);
+		p3.getCategory().add(cat3);
+		p4.getCategory().add(cat3);
+		p5.getCategory().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		User user1 = new User(null, "Marcos", "marcos@gmail.com", "9999-9999", "123456");
 		User user2 = new User(null, "John", "john@gmail.com", "8888-8888", "987654321");
@@ -57,20 +70,8 @@ public class Config implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-
-		p1.getCategory().add(cat2);
-		p2.getCategory().add(cat1);
-		p2.getCategory().add(cat3);
-		p3.getCategory().add(cat3);
-		p4.getCategory().add(cat3);
-		p5.getCategory().add(cat2);
-
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
@@ -78,6 +79,12 @@ public class Config implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 
 	}
 
